54e9c6b760b00c003384079a002c3d38bcdaad39
Kyplik <diman.smirny@yandex.ru>
2025-04-30 21:22:07 +0400

    Lesson_14_testng

---
+++ src/main/java/Lesson_14_testng/FactorialCalculator.java
@@ -0,0 +1,14 @@
+package Lesson_14_testng;
+
+public class FactorialCalculator {
+    public static long calculateFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Факториал не определяется для отрицательных чисел");
+        }
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
